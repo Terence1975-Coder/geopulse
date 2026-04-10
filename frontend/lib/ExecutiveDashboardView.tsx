2309ae9d7f1b338db4200dedc916f7d4fa4accbd
Terence Steaming Herbert <syneventconsult@gmail.com>
2026-04-10 08:44:22 +0100

    Remove legacy lib ExecutiveDashboardView duplicate

--- frontend/lib/ExecutiveDashboardView.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardPanel from "../DashboardPanel";
-import SignalFeedCard from "../SignalFeedCard";
-import ProfileCompletenessCard from "../ProfileCompletenessCard";
-import {
-  CompanyProfile,
-  DashboardSummary,
-  OpportunityItem,
-  SignalItem,
-} from "../../types/geopulse";
